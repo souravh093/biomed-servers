18218052d3d0e7662f7ecd15d52451cc7a686ec3
Sourave halder <souravh093@gmail.com>
2023-08-15 18:02:07 +0600

    some change the branch

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;  
 
 app.use(cors());
 app.use(express.json());
