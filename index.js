048e33502f7d0bcbe6f2123cb24f9d2ae19b4d2a
rh-abir <rhabir.co@gmail.com>
2023-09-09 00:29:10 +0600

    solve error

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 require("dotenv").config();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const port = process.env.PORT || 5000;
 
@@ -15,19 +15,23 @@ app.use(express.json());
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
-    return res.status(401).send({ error: true, message: 'unauthorized access' });
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
   }
   // bearer token
-  const token = authorization.split(' ')[1];
+  const token = authorization.split(" ")[1];
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(401).send({ error: true, message: 'unauthorized access' })
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
     }
     req.decoded = decoded;
     next();
-  })
-}
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.2sex9a1.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -52,28 +56,33 @@ async function run() {
       .db("biomedDB")
       .collection("appliedjobs");
     const blogsCollection = client.db("biomedDB").collection("blogs");
-    const SocialMediaCollection = client.db("biomedDB").collection("social-media");
+    const SocialMediaCollection = client
+      .db("biomedDB")
+      .collection("social-media");
     const applicantsCollection = client.db("biomedDB").collection("applicants");
     const testimonialsCollection = client
-    .db("biomedDB")
-    .collection("testimonials");
-    
+      .db("biomedDB")
+      .collection("testimonials");
 
-    app.post('/jwt', (req, res) => {
+    app.post("/jwt", (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-      res.send({ token })
-    })
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
 
     const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
-      const query = { email: email }
+      const query = { email: email };
       const user = await usersCollection.findOne(query);
       if (user?.admin !== true && user?.moderator !== true) {
-          return res.status(403).send({ error: true, message: 'forbidden message' });
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden message" });
       }
       next();
-    }
+    };
 
     // save user in database with email and role
     app.put("/users/:email", async (req, res) => {
@@ -422,12 +431,11 @@ async function run() {
       const result = await applidejobsCollection.deleteOne(query);
       res.send(result);
     });
-  // applicants
-  app.get("/applicants", async (req, res) => {
-    const result = await applicantsCollection.find().toArray();
-    res.send(result);
-  });
-
+    // applicants
+    app.get("/applicants", async (req, res) => {
+      const result = await applicantsCollection.find().toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
